de998c669b82dcca050f5bcab291ee0ef59ee609
DavidCiccone <davidkciccone@gmail.com>
2020-02-06 21:03:12 -0500

    Final
    final code before review.

--- a_random_quote_generator-v1/inc/functions.php
+++ a_random_quote_generator-v1/inc/functions.php
@@ -1,58 +1,64 @@
-// PHP - Random Quote Generator
-
-// Create the Multidimensional array of quote elements and name it quotes
-
-
-
-// Each inner array element should be an associative array
-
-
-
-// Create the getRandomQuuote function and name it getRandomQuote
-
-
-
-// Create the printQuote funtion and name it printQuotewe
-
-//Quote, Source, Citation, Year
 <?php
-$quotes = array(
-    array(
-        "quote" => "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.","source" => "Patrick McKenzie",
-        "citation" => "Twitter",
-        "year" => "2016"
-    ),
-    array(
-        "quote" => "Die with memories, not dreams.",
-        "source" => "",
-        "citation" => "codeofliving.com",
-        "year" => ""
-    ),
-    array(
-        "quote" => "Everything you can imagine is real.",
-        "source" => "Pablo Picasso",
-        "citation" => "codeofliving.com",
-        "year" => ""
-    ),
-    array(
-        "quote" => "I've failed over and over and over again in my life and that is why I succeed.",
-        "source" => "Michael Jordan",
-        "citation" => "Nike TV commercial",
-        "year" => "1997"
-    ),
-    array(
-        "quote" => "Small steps in the right direction can turn out to be the biggest step of your life.",
-        "source" => "Unknown",
-        "citation" => "",
-        "year" => ""
-    )
-  );
-//var_dump($quotes[1]['quote']);
+//refreshes the page every 20 seconds
+header("Refresh:20");
 
-    function getRandomQuote($array){
-       
-        echo($array[rand(0,4)]['quote']);
+//array of quote information
+    $quotes = array(
+        array(
+            "quote" => "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.","source" => "Patrick McKenzie",
+            "citation" => "Twitter",
+            "year" => "2016"
+        ),
+        array(
+            "quote" => "Die with memories, not dreams.",
+            "source" => "Unknown",
+            "citation" => "codeofliving.com",
+            "year" => ""
+        ),
+        array(
+            "quote" => "Everything you can imagine is real.",
+            "source" => "Pablo Picasso",
+            "citation" => "codeofliving.com",
+            "year" => ""
+        ),
+        array(
+            "quote" => "I've failed over and over and over again in my life and that is why I succeed.",
+            "source" => "Michael Jordan",
+            "citation" => "Nike TV commercial",
+            "year" => "1997"
+        ),
+        array(
+            "quote" => "Small steps in the right direction can turn out to be the biggest step of your life.",
+            "source" => "Unknown",
+            "citation" => "",
+            "year" => ""
+        )
+    );
+
+    //function that chooses a random quote from the above array
+    function getRandomQuote($array){    
+        return $array[rand(0,4)];
     }
-    getRandomQuote($quotes);
+    
+    //function that prints the quote
+    function printQuote($array){
+        
+        $singleQuote = getRandomQuote($array);
+         
+        //builds the string to apply to the page
+        $quoteString = '<p class="quote">'. $singleQuote["quote"] .'</p><p class="source">'. $singleQuote["source"];
+        
+        if($singleQuote["citation"] && $singleQuote["year"]){
+           $quoteString .= '<span class="citation">' . $singleQuote["citation"] . '</span><span class="year">' . $singleQuote["year"] . '</span></p>';
+        } else if($singleQuote["citation"]){
+            $quoteString .= '<span class="citation">' . $singleQuote["citation"] . '</span></p>';
+        }else if($singleQuote["year"]){
+            $quoteString .= '<span class="year">' . $singleQuote["year"] . '</span></p>';
+        }else{
+            $quoteString .= '</p>';
+        };
+        //prints the string to page      
+        print $quoteString;            
+    };
+    printQuote($quotes);
 ?>
-  
