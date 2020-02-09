3904f20cd6c64910c3b4fa259f828671368771e5
DavidCiccone <davidkciccone@gmail.com>
2020-02-09 18:18:32 -0500

    Edit: change to array.

--- a_random_quote_generator-v1/inc/functions.php
+++ a_random_quote_generator-v1/inc/functions.php
@@ -7,31 +7,36 @@ header("Refresh:20");
         array(
             "quote" => "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.","source" => "Patrick McKenzie",
             "citation" => "Twitter",
-            "year" => "2016"
+            "year" => "2016",
+            "catagory" => array("tag" => "random")
         ),
         array(
             "quote" => "Die with memories, not dreams.",
             "source" => "Unknown",
             "citation" => "codeofliving.com",
-            "year" => ""
+            "year" => "",
+            "catagory" => array("tag" => "random")
         ),
         array(
             "quote" => "Everything you can imagine is real.",
             "source" => "Pablo Picasso",
             "citation" => "codeofliving.com",
-            "year" => ""
+            "year" => "",
+            "catagory" => array("tag" => "random")
         ),
         array(
             "quote" => "I've failed over and over and over again in my life and that is why I succeed.",
             "source" => "Michael Jordan",
             "citation" => "Nike TV commercial",
-            "year" => "1997"
+            "year" => "1997",
+            "catagory" => array("tag" => "inspirational")
         ),
         array(
             "quote" => "Small steps in the right direction can turn out to be the biggest step of your life.",
             "source" => "Unknown",
             "citation" => "",
-            "year" => ""
+            "year" => "",
+            "catagory" => array("tag" => "inspirational")
         )
     );
 
